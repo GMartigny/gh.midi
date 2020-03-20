75b24d3fe97885cc8e7ef01dd2c8cc06efc74f14
Guillaume Martigny <guillaume.martigny@gmail.com>
2020-03-20 09:58:38 +0100

    Use new "now" directory tree

---
+++ api/[username].js
@@ -0,0 +1,29 @@
+const allow = require("allow-cors");
+const GHMidi = require("../index");
+
+module.exports = async (request, response) => {
+    const { username } = request.query;
+
+    allow(response);
+
+    if (!username) {
+        response.statusCode = 500;
+        response.end("Username required");
+        return;
+    }
+
+    try {
+        const bytes = await GHMidi(username);
+
+        response.statusCode = 200;
+        response.setHeader("Content-Type", "audio/midi");
+        response.setHeader("Content-Disposition", `attachment;filename=${username}.mid`);
+        response.write(Buffer.from(bytes, "binary"));
+    }
+    catch ({ message }) {
+        response.statusCode = 500;
+        response.write(message);
+    }
+
+    response.end();
+};
